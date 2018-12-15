c647bdac5599a4b040391d1d523cebcabfc8b13d
Online Web Tutor <onlinewebtutorhub@gmail.com>
2018-12-15 06:12:08 +0530

    Vue JS APIS & package: axios

---
+++ vueapis/database.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `students` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `name` varchar(255) DEFAULT NULL,
+ `email` varchar(100) DEFAULT NULL,
+ `phone_no` varchar(15) DEFAULT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
