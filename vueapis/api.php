d9a24562a3c6df475cc77d107c5a7be9873401f6
Online Web Tutor <onlinewebtutorhub@gmail.com>
2018-12-18 06:28:03 +0530

    Insert & update functions added

--- vueapis/api.php
+++ vueapis/api.php
@@ -2,18 +2,64 @@
 
    include './config.php';
 
+   // header functions to allow these apis consumed by Yaf_Application
+   header("Access-Control-Allow-Origin: *");
+   header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
    $method = $_SERVER['REQUEST_METHOD'];
 
    if($method=="POST"){
       // this is for insert data
+      if($action=="insert"){
+         // insertion
+         $bodyParams = json_decode(file_get_contents("php://input"),true);
+         // $bodyParams->email => $bodyParams['email']
+         $name = isset($bodyParams['name']) ? $bodyParams['name'] :"";
+         $email = isset($bodyParams['email']) ? $bodyParams['email'] :"";
+         $phone = isset($bodyParams['phone']) ? $bodyParams['phone'] :"";
+
+         $insertQuery = "Insert into students (name, email, phone_no) values ('".$name."','".$email."','".$phone."')";
+
+         if($connection->query($insertQuery)===TRUE){
+             echo json_encode(array(
+               "status"=>1,
+               "message"=>"Values has been inserted"
+             ));
+         }else{
+           echo json_encode(array(
+             "status"=>0,
+             "message"=>"Failed to insert value"
+           ));
+         }
+      }else if($action=="update"){
+          // updation
+          $bodyParams = json_decode(file_get_contents("php://input"),true);
+          // $bodyParams->email => $bodyParams['email
+          $student_id = isset($bodyParams['student_id']) ? intval($bodyParams['student_id']) :"";
+          $name = isset($bodyParams['name']) ? $bodyParams['name'] :"";
+          $email = isset($bodyParams['email']) ? $bodyParams['email'] :"";
+          $phone = isset($bodyParams['phone']) ? $bodyParams['phone'] :"";
+
+          $updateQuery = "Update students SET name = '".$name."', email = '".$email."', phone_no = '".$phone."' WHERE id = ".$student_id;
+
+          if($connection->query($updateQuery)===TRUE){
+              echo json_encode(array(
+                "status"=>1,
+                "message"=>"Record has been updated"
+              ));
+          }else{
+            echo json_encode(array(
+              "status"=>0,
+              "message"=>"Failed to update record"
+            ));
+          }
+      }
    }else if($method=="GET"){
 
        $action = isset($_GET['action']) ? $_GET['action']: "";
 
        if($action == "list"){
            // this is listing of all data
-       }else if($action == "edit"){
-           // this is for edit operation
        }else if($action == "delete"){
            // this is for delete operation
        }
